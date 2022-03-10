6b495c78b8d8d3008476c143f823f43be87f0864
Yago Batista <yagobatistafis@yahoo.com.br>
2022-03-10 20:48:17 -0300

    the end? kkk

--- components/Wrapper.js
+++ components/Wrapper.js
@@ -1,25 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { Box, Flex, Text, VStack } from "@chakra-ui/react";
-import useFetch from "../hook/useFetch";
+import { Flex, Text, VStack } from "@chakra-ui/react";
 import Image from "next/image";
-import test from "../styles/images/pattern-divider-desktop.svg";
+import dividerDesktop from "../styles/images/pattern-divider-desktop.svg";
 import diceIcon from "../styles/images/icon-dice.svg";
 
 const Wrapper = () => {
   const [touched, setTouched] = useState(false);
-  const { loadedData, loading } = useFetch("https://api.adviceslip.com/advice");
+  const [className, setClassName] = useState("");
+  const [nextFetch, setNextFetch] = useState(1);
+  const [loadedData, setLoadedData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (touched) {
-      setTimeout(() => {
-        console.log("cu");
-      }, 1000);
-    }
+    setClassName("rotate");
+    fetch("https://api.adviceslip.com/advice")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) =>
+        setTimeout(() => {
+          setLoadedData(data);
+        }, 3000)
+      )
+      .catch((error) => setError(error));
 
-    return () => {
-      clearTimeout(() => console.log("oi"));
-    };
-  }, [touched]);
+    setTouched(true);
+    setTimeout(() => {
+      setClassName("");
+      setTouched(false);
+    }, 4000);
+  }, [nextFetch]);
 
   return (
     <VStack
@@ -30,18 +42,38 @@ const Wrapper = () => {
       w="80%"
       maxW="550px"
       h="fit-content"
+      minH="300px"
       borderRadius="15px"
-      p="3%"
     >
-      <Flex color="hsl(150, 100%, 66%)" py="1%">
+      <Flex color="hsl(150, 100%, 66%)">
         <Text>ADVICE #</Text>
         <Text textAlign="center">{loadedData?.slip.id}</Text>
       </Flex>
-      <Text pb="6%">"{loadedData?.slip.advice}"</Text>
-      <Image src={test} />
+      <Text
+        fontSize={["20px", "24px", "28px"]}
+        maxW="95%"
+        textAlign="center"
+        pb="6%"
+      >
+        "{loadedData?.slip.advice}"
+      </Text>
+      {/* <Flex
+        position="absolute"
+        bottom="15%"
+        justifyContent="center"
+        alignItems={"center"}
+        objectFit="cover"
+      >
+        <Image src={test} />
+      </Flex> */}
+      <Flex position={"absolute"} maxW="80%" bottom="15%" pb="3%">
+        <Image src={dividerDesktop} />
+      </Flex>
       <Flex
-        onClick={() => setTouched(true)}
-        className={touched ? "test" : ""}
+        onClick={() => {
+          if (!touched) setNextFetch((prev) => prev + 1);
+        }}
+        className={className}
         position="absolute"
         bottom="-22.5px"
         left="calc(50% - 22.5px)"
